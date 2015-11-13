8939390fc53bbd2d663dcdfdd3e3bcec456eb35e
Per Gröningsson <per.groningsson@vattenfall.com>
2015-11-13 15:25:07 +0100

    updated readfile.py

--- readfile.py
+++ readfile.py
@@ -5,6 +5,10 @@ from IPython.core.debugger import Tracer
 # Inside iptyhon shell: run -d b<L> readfile.py
 # sets a break point add line L.
 
+# For plotting
+import matplotlib.pyplot as plt
+
+
 import numpy as np
 import re
 import linecache
@@ -29,7 +33,7 @@ def readfile():
     reBWR = re.compile('^\s*BWR')
     reLFU = re.compile('^\s*LFU')
     reLPI = re.compile('^\s*LPI')
-    reFUE = re.compile('^\s*FUE')
+    reFUE = re.compile('^\s*FUE\s+')
     reTTL = re.compile('^\s*TTL')
     reSIM = re.compile('^\s*SIM')
     rePDE = re.compile('^\s*PDE')
@@ -44,15 +48,31 @@ def readfile():
     reTMO = re.compile('^\s*TMO')
     reTFU = re.compile('^\s*TFU')
     reVOI = re.compile('^\s*VOI')
-    
-    # Setup empty integer array
+    reS3C = re.compile('(^| )S3C')
+    reTIT = re.compile('^TIT')
+    reTTL = re.compile('^\*I TTL')
+    reREA = re.compile('REA\s+')
+    reGPO = re.compile('GPO\s+')
+    rePOW = re.compile('POW\s+')
+
+    # Setup empty integer arrays
+    iEND = np.arange(0,dtype='int32')
     iBWR = np.arange(0,dtype='int32')
     iLFU = np.arange(0,dtype='int32')
     iLPI = np.arange(0,dtype='int32')
     iFUE = np.arange(0,dtype='int32')
+    iPIN = np.arange(0,dtype='int32')
+    iTIT = np.arange(0,dtype='int32')
+    iTTL = np.arange(0,dtype='int32')
+    iREA = np.arange(0,dtype='int32')
+    iGPO = np.arange(0,dtype='int32')
+    iPOW = np.arange(0,dtype='int32')
 
+    # Search for regexp matches
     for i, line in enumerate(flines):
-        if reBWR.match(line) is not None:
+        if reEND.match(line) is not None:
+            iEND = np.append(iEND,i)
+        elif reBWR.match(line) is not None:
             iBWR = np.append(iBWR,i)
         elif reLFU.match(line) is not None:
             iLFU = np.append(iLFU,i)
@@ -60,6 +80,18 @@ def readfile():
             iLPI = np.append(iLPI,i)
         elif reFUE.match(line) is not None:
             iFUE = np.append(iFUE,i)
+        elif rePIN.match(line) is not None:
+            iPIN = np.append(iPIN,i)
+        elif reTIT.match(line) is not None:
+            iTIT = np.append(iTIT,i)
+        elif reTTL.match(line) is not None:
+            iTTL = np.append(iTTL,i)
+        elif reREA.match(line) is not None:
+            iREA = np.append(iREA,i)
+        elif reGPO.match(line) is not None:
+            iGPO = np.append(iGPO,i)
+        elif rePOW.match(line) is not None:
+            iPOW = np.append(iPOW,i)
 
     # Read fuel dimension
     npst = int(flines[iBWR[0]][5:7])
@@ -68,66 +100,92 @@ def readfile():
     caxmap = flines[iLFU[0]+1:iLFU[0]+1+npst]
     LFU = symtrans(map2mat(caxmap,npst)).astype(int)
 
+    # Determine fuel type
+    
+
+
     # Read LPI map
     caxmap = flines[iLPI[0]+1:iLPI[0]+1+npst]
     LPI = symtrans(map2mat(caxmap,npst)).astype(int)
 
     # Read FUE
+    iFUE = iFUE[iFUE<iEND[0]]
+    Nfue = iFUE.size
+    FUE = np.zeros((Nfue,5)); FUE.fill(np.nan)
+    for i,idx in enumerate(iFUE):
+        rvec = re.split('\*',flines[idx].strip())
+        rstr = rvec[0]
+        rvec = re.split('\s+',rstr.strip())
+        FUE[i,0] = rvec[1]
+        FUE[i,1:3] = re.split('/',rvec[2])
+        if np.size(rvec) > 3:
+            FUE[i,3:5] = re.split('=',rvec[3])
+
+    # Determine number of BA rods types
+    Nba = 0
+    for content in FUE[:,4]:
+        if np.isnan(content) == False:
+            Nba += 1
+
+    # Read PIN
+    Npin = iPIN.size
+    
+    # Read SLA
+    
+    
+
+#    # Check if S3C card exists
+#    S3C = False
+#    for i,line in enumerate(flines):
+#        if reS3C.match(line) is not None:
+#            iS3C = i
+#            S3C = True
+#            break
     
 
 
+    # Find the lines containing some key cards
+    #reTIT = re.compile('^TIT')
+    #reTTL = re.compile('^\*I TTL')
+    #reREA = re.compile('REA\s+')
+    #reGPO = re.compile('GPO\s+')
+    #rePOW = re.compile('POW\s+')
 
-#    reBWR = re.compile('^\s*BWR')
-#    for line in flines:
-#        if reBWR.match(line) is not None:
-#            fuedim = int(line[5:7])
-#            break
+    #iTIT = np.array([])
+    #iTIT = np.arange(0,dtype='int32') # Setup empty integer array
+    #iTIT = np.zeros(100000,dtype='int32')
+    #iTTL = np.arange(0,dtype='int32')
+    #iREA = np.arange(0,dtype='int32')
+    #iGPO = np.arange(0,dtype='int32')
+    #iPOW = np.arange(0,dtype='int32')
 
+    #for idx, line in enumerate(flines):
+    #    #if reTIT.match(line) is not None:
+    #    #    iTIT = np.append(iTIT,idx)
+    #    elif reTTL.match(line) is not None:
+    #        iTTL = np.append(iTTL,idx)
+    #    elif reREA.match(line) is not None:
+    #        iREA = np.append(iREA,idx)
+    #    elif reGPO.match(line) is not None:
+    #        iGPO = np.append(iGPO,idx)
+    #    elif rePOW.match(line) is not None:
+    #        iPOW = np.append(iPOW,idx)
+        #if idx == 50000: # Max number of rows to scan
+        #    break
 
-    Tracer()()
+    # Calculate burnup points
 
     # Check if S3C card exists
     S3C = False
-    reS3C = re.compile('(^| )S3C')
     for i,line in enumerate(flines):
         if reS3C.match(line) is not None:
             iS3C = i
             S3C = True
             break
-    
-    # Find the lines containing some key cards
-    reTIT = re.compile('^TIT')
-    reTTL = re.compile('^\*I TTL')
-    reREA = re.compile('REA\s+')
-    reGPO = re.compile('GPO\s+')
-    rePOW = re.compile('POW\s+')
 
-    #iTIT = np.array([])
-    iTIT = np.arange(0,dtype='int32') # Setup empty integer array
-    #iTIT = np.zeros(100000,dtype='int32')
-    iTTL = np.arange(0,dtype='int32')
-    iREA = np.arange(0,dtype='int32')
-    iGPO = np.arange(0,dtype='int32')
-    iPOW = np.arange(0,dtype='int32')
-
-    for idx, line in enumerate(flines):
-        if reTIT.match(line) is not None:
-            iTIT = np.append(iTIT,idx)
-        elif reTTL.match(line) is not None:
-            iTTL = np.append(iTTL,idx)
-        elif reREA.match(line) is not None:
-            iREA = np.append(iREA,idx)
-        elif reGPO.match(line) is not None:
-            iGPO = np.append(iGPO,idx)
-        elif rePOW.match(line) is not None:
-            iPOW = np.append(iPOW,idx)
-        #if idx == 50000: # Max number of rows to scan
-        #    break
-
-    if iGPO.size >= iREA.size:
+    if iGPO.size >= iREA.size: # Use gamma smearing power
         iPOW = iGPO
 
-    # Calculate burnup points
     if S3C:
         tmpvec = iTIT[iTIT>iTTL[1]]
         tmpvec = tmpvec[tmpvec<iTTL[2]]
@@ -148,23 +206,27 @@ def readfile():
 
     # Read burnup and kinf
     burnup = np.zeros(Nburnpts); burnup.fill(np.nan)
-    kinf = np.zeros(Nburnpts); kinf.fill(np.nan)
+    Kinf = np.zeros(Nburnpts); Kinf.fill(np.nan)
     for i in range(Nburnpts):
         burnup[i] = flines[iTITbp[i]+2][0:6]
-        kinf[i] = flines[iREAbp[i]+1][0:12]
+        Kinf[i] = flines[iREAbp[i]+1][0:12]
     
     
     # Read radial power distribution map
 
-    POW = np.zeros((fuedim,fuedim,Nburnpts)); POW.fill(np.nan)
+    POW = np.zeros((npst,npst,Nburnpts)); POW.fill(np.nan)
     for i in range(Nburnpts):
-        caxmap = flines[iPOW[i]+2:iPOW[i]+2+fuedim]
-        M = map2mat(caxmap,fuedim)
-        M = symtrans(M)
+        caxmap = flines[iPOW[i]+2:iPOW[i]+2+npst]
+        M = symtrans(map2mat(caxmap,npst))
         POW[:,:,i] = M
 
+    # Calculate Fint
+    Fint = np.zeros(Nburnpts); Fint.fill(np.nan)
+    for i in range(Nburnpts):
+        Fint[i] = POW[:,:,i].max()
+
     # Calculate radial burnup distribution
-    EXP = np.zeros((fuedim,fuedim,Nburnpts)); EXP.fill(np.nan)
+    EXP = np.zeros((npst,npst,Nburnpts)); EXP.fill(np.nan)
     EXP[:,:,0] = 0 # Initial burnup
     for i in range(1,Nburnpts):
         dburn = burnup[i] - burnup[i-1]
