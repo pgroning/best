f300c5fe85b3bba0c351c2d327f1fab36225b096
Per Gröningsson <per.groningsson@gmail.com>
2015-12-05 22:30:22 +0100

    modified casobjrw.py

--- casobjrw.py
+++ casobjrw.py
@@ -1,14 +1,17 @@
-import pickle
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 from casdata_pts import casdata
 
 x = casdata('cax/e29OPT2-389-10g40mid-cas.cax')
 
-fname = 'casobj.pic'
+fname = 'casobj.p'
 # Write object to file
 print "Write data to file..."
 with open(fname,'wb') as fp:
-    pickle.dump(x,fp)
+    pickle.dump(x,fp,1)
     
 # Read object back from file
 print "Read data from file..."
