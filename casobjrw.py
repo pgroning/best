ef079626cfe2c0ade2f0d3f5ad4a9502c12b1fbb
Per Gröningsson <per.groningsson@gmail.com>
2015-12-06 20:59:12 +0100

    modified casobjrw.py

--- casobjrw.py
+++ casobjrw.py
@@ -1,3 +1,5 @@
+from IPython.core.debugger import Tracer
+
 try:
     import cPickle as pickle
 except:
@@ -6,13 +8,19 @@ except:
 from casdata_pts import casdata
 
 x = casdata('cax/e29OPT2-389-10g40mid-cas.cax')
+x2 = casdata('cax/e29OPT2-389-10g40mid-cas.cax')
+x3 = casdata('cax/e29OPT2-389-10g40mid-cas.cax')
+xlist = []
+xlist.append(x)
+xlist.append(x2)
+xlist.append(x3)
 
 fname = 'casobj.p'
 # Write object to file
 print "Write data to file..."
 with open(fname,'wb') as fp:
-    pickle.dump(x,fp,1)
-    
+    pickle.dump(xlist,fp,1)
+
 # Read object back from file
 print "Read data from file..."
 with open(fname,'rb') as fp:
