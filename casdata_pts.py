c54931cc70ceb0d7065decf99e7c298f1b7414a5
Per Gröningsson <per.groningsson@vattenfall.com>
2015-12-08 10:57:51 +0100

    updated data storage in casdata_pts.py

--- casdata_pts.py
+++ casdata_pts.py
@@ -21,14 +21,15 @@ import sys
 
 #from btf import btf
 
-class data:
+class datastruct: # dummy class used to structure data
     pass
+
 
 class casdata:
 
     def __init__(self,caxfile):
         self.readcax(caxfile)
-        #self.ave_enr()
+        self.__ave_enr()
         #self.writecai()
         #self.btfcalc()
 
@@ -117,46 +118,46 @@ class casdata:
         iSTA = next(i for i,x in enumerate(flines) if reSTA.match(x))
         print "Done."
 
-        info = data()
+        self.data = datastruct()
         # Read title
         #self.title = flines[iTTL[0]]
-        info.title = flines[iTTL[0]]
+        self.data.title = flines[iTTL[0]]
         # SIM
         #self.sim = flines[iSIM[0]]
-        info.sim = flines[iSIM[0]]
+        self.data.sim = flines[iSIM[0]]
         # TFU
         #self.tfu = flines[iTFU]
-        info.tfu = flines[iTFU]
+        self.data.tfu = flines[iTFU]
         # TMO
         #self.tmo = flines[iTMO]
-        info.tmo = flines[iTMO]
+        self.data.tmo = flines[iTMO]
         # VOI
         #self.voi = flines[iVOI[0]]
-        info.voi = flines[iVOI[0]]
+        self.data.voi = flines[iVOI[0]]
         # PDE
         #self.pde = flines[iPDE]
-        info.pde = flines[iPDE]
+        self.data.pde = flines[iPDE]
         # BWR
         #self.bwr = flines[iBWR]
-        info.bwr = flines[iBWR]
+        self.data.bwr = flines[iBWR]
         # SPA
         #self.spa = flines[iSPA[0]]
-        info.spa = flines[iSPA[0]]
+        self.data.spa = flines[iSPA[0]]
         # DEP
         #self.dep = flines[iDEP[0]]
-        info.dep = flines[iDEP[0]]
+        self.data.dep = flines[iDEP[0]]
         # GAM
         #self.gam = flines[iGAM[0]]
-        info.gam = flines[iGAM[0]]
+        self.data.gam = flines[iGAM[0]]
         # WRI
         #self.wri = flines[iWRI]
-        info.wri = flines[iWRI]
+        self.data.wri = flines[iWRI]
         # STA
         #self.sta = flines[iSTA]
-        info.sta = flines[iSTA]
+        self.data.sta = flines[iSTA]
         # CRD
         #self.crd = flines[iCRD[0]]
-        info.crd = flines[iCRD[0]]
+        self.data.crd = flines[iCRD[0]]
 
         # Read fuel dimension
         npst = int(flines[iBWR][5:7])
@@ -219,21 +220,21 @@ class casdata:
             PIN[i,:rlen-1] = rvec[1:ncol+1]
 
         #self.pinlines = flines[iPIN[0]:iPIN[0]+Npin]
-        info.pinlines = flines[iPIN[0]:iPIN[0]+Npin]
+        self.data.pinlines = flines[iPIN[0]:iPIN[0]+Npin]
 
         # Read SLA
         #Nsla = iSLA.size
         #self.slalines = flines[iSLA[0]:iSLA[0]+Nsla]
         #self.slalines = flines[iSLA]
-        info.slalines = flines[iSLA]
+        self.data.slalines = flines[iSLA]
 
         # ------Step through the state points----------
         print "Step through state points..."
 
         # Remove the last TIT card (only necessary if file is partially read)
         #iTIT = iTIT[0:-1]
-
-        self.data = {}
+        
+        #self.statepts = datastruct()
         
         #state.nstatepoints = len(iTIT)
 
@@ -288,32 +289,56 @@ class casdata:
             fint[i] = POW[:,:,i].max()
 
         # Append state
-        self.data['statepts'] = []
+        self.statepts = [] # statepts is a list
+        #self.data['statepts'] = []
+        #Tracer()()
         for i in range(Nburnpts):
-            state = data()
-            state.burnup = burnup[i]
-            state.voi = voi[i]
-            state.vhi = vhi[i]
-            state.tfu = tfu[i]
-            state.tmo = tmo[i]
-            state.kinf = kinf[i]
-            state.fint = fint[i]
-            state.EXP = EXP[:,:,i]
-            state.XFL1 = XFL1[:,:,i]
-            state.XFL2 = XFL2[:,:,i]
-            state.POW = POW[:,:,i]
-            self.data["statepts"].append(state)
-            
+            self.statepts.append(datastruct()) # append new instance to list
+            #state = data()
+            self.statepts[i].burnup = burnup[i]
+            #state.burnup = burnup[i]
+            self.statepts[i].voi = voi[i]
+            #state.voi = voi[i]
+            self.statepts[i].vhi = vhi[i]
+            #state.vhi = vhi[i]
+            self.statepts[i].tfu = tfu[i]
+            #state.tfu = tfu[i]
+            self.statepts[i].tmo = tmo[i]
+            #state.tmo = tmo[i]
+            self.statepts[i].kinf = kinf[i]
+            #state.kinf = kinf[i]
+            self.statepts[i].fint = fint[i]
+            #state.fint = fint[i]
+            self.statepts[i].EXP = EXP[:,:,i]
+            #state.EXP = EXP[:,:,i]
+            self.statepts[i].XFL1 = XFL1[:,:,i]
+            #state.XFL1 = XFL1[:,:,i]
+            self.statepts[i].XFL2 = XFL2[:,:,i]
+            #state.XFL2 = XFL2[:,:,i]
+            self.statepts[i].POW = POW[:,:,i]
+            #state.POW = POW[:,:,i]
+            #self.data["statepts"].append(state)
+        
+        #Tracer()()
         # Saving geninfo
-        info.caxfile = caxfile
-        info.ENR = ENR
-        info.BA = BA
-        info.PIN = PIN
-        info.LPI = LPI
-        info.FUE = FUE
-        info.LFU = LFU
-        info.npst = npst
-        self.data['geninfo'] = info
+        #self.geninfo = data()
+        self.data.caxfile = caxfile
+        #info.caxfile = caxfile
+        self.data.ENR = ENR
+        #info.ENR = ENR
+        self.data.BA = BA
+        #info.BA = BA
+        self.data.PIN = PIN
+        #info.PIN = PIN
+        self.data.LPI = LPI
+        #info.LPI = LPI
+        self.data.FUE = FUE
+        #info.FUE = FUE
+        self.data.LFU = LFU
+        #info.LFU = LFU
+        self.data.npst = npst
+        #info.npst = npst
+        #self.data['geninfo'] = info
 
         #self.caxfile = caxfile
         #self.burnvec = burnup
@@ -366,29 +391,29 @@ class casdata:
 #        self.fint = fint
 
     # --------Calculate average enrichment----------
-    def ave_enr(self):
-
+    def __ave_enr(self):
+        
         # Translate LFU map to density map
-        DENS = np.zeros((self.npst,self.npst));
-        Nfue = self.FUE[:,0].size
+        DENS = np.zeros((self.data.npst,self.data.npst));
+        Nfue = self.data.FUE[:,0].size
         for i in range(Nfue):
-            ifu = int(self.FUE[i,0])
-            DENS[self.LFU==ifu] = self.FUE[i,1]
- 
+            ifu = int(self.data.FUE[i,0])
+            DENS[self.data.LFU==ifu] = self.data.FUE[i,1]
+        
         # Translate LPI map to pin radius map
-        RADI = np.zeros((self.npst,self.npst));
-        Npin = self.PIN[:,0].size
+        RADI = np.zeros((self.data.npst,self.data.npst));
+        Npin = self.data.PIN[:,0].size
         for i in range(Npin):
-            ipi = int(self.PIN[i,0])
-            RADI[self.LPI==ipi] = self.PIN[i,1]
+            ipi = int(self.data.PIN[i,0])
+            RADI[self.data.LPI==ipi] = self.data.PIN[i,1]
         
         # Calculate mass
         VOLU = np.pi*RADI**2
         MASS = DENS*VOLU
         mass = np.sum(MASS)
-        MASS_U235 = MASS*self.ENR
+        MASS_U235 = MASS*self.data.ENR
         mass_u235 = np.sum(MASS_U235)
-        self.ave_enr = mass_u235/mass
+        self.data.ave_enr = mass_u235/mass
 
 
     # -------Write cai file------------
