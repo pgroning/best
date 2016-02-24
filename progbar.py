de1399fd2b12e3fecfe464d3da77c209cc53df7d
Per Gröningsson <per.groningsson@gmail.com>
2016-02-24 22:29:49 +0100

    modified progbar.py

--- progbar.py
+++ progbar.py
@@ -6,19 +6,24 @@ from PyQt4.QtGui import *
 
 
 class ProgressBar(QWidget):
-    def __init__(self, parent=None, total=20):
+    def __init__(self, parent=None, total=100):
         super(ProgressBar, self).__init__(parent)
         self.progressbar = QProgressBar()
-        self.progressbar.setMinimum(1)
+        self.progressbar.setMinimum(0)
         self.progressbar.setMaximum(total)
+        self.button = QPushButton('Cancel')
         main_layout = QGridLayout()
         main_layout.addWidget(self.progressbar, 0, 0)
+        main_layout.addWidget(self.button, 1, 0)
         self.setLayout(main_layout)
         self.setWindowTitle('Progress')
         self.resize(200,50)
         self.move(500,500)
+        self.setWindowModality(True)
         #Tracer()()
 
+    def update(self,val):
+        self.progressbar.setValue(val)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
